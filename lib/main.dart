0ccf4c021cd5efcd3981765f9081c2c09db9b227
Beshoy Armia <beshoarmia0@gmail.com>
2025-11-01 11:58:45 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Note_app());
+}
+
+class Note_app extends StatelessWidget {
+  const Note_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Test App')),
+        body: const Center(child: Text('Hello,fhfsf!')),
+      ),
+    );
+  }
+}
